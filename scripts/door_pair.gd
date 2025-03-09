3aa45adecf368904ba4e9bee81c5d80ea2ed327a
bernardoup31 <up202108842@up.pt>
2025-03-09 16:58:12 +0000

    Doors done, need sprite for it

---
+++ scripts/door_pair.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var layerDoor1 := 1
+@export var layerDoor2 := 2
+
+
+@onready var Door1 := $Door1
+@onready var Door2 := $Door2
+
+var height := 230
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	Door1.position.y = layerDoor1 * height
+	Door2.position.y = layerDoor2 * height
+	
+	Door1.setTargetDoor(Door2)
+	Door2.setTargetDoor(Door1)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
