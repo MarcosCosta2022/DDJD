05e992f803846e6785ba24d5e69f7f36249e4cc8
Bernardo Pinto <up202108842@up.pt>
2025-03-11 19:27:25 +0000

    Scene rescaled

--- scripts/door_pair.gd
+++ scripts/door_pair.gd
@@ -7,18 +7,12 @@ extends Node2D
 @onready var Door1 := $Door1
 @onready var Door2 := $Door2
 
-var height := 183
+var height := 206
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if(layerDoor1 != 3):
-		Door1.position.y = layerDoor1 * height
-	else: 
-		Door1.position.y = layerDoor1 * height + 20
-	if(layerDoor2 != 3):
-		Door2.position.y = layerDoor2 * height
-	else: 
-		Door2.position.y = layerDoor2 * height + 24
+	Door1.position.y = layerDoor1 * height
+	Door2.position.y = layerDoor2 * height
 	
 	Door1.setTargetDoor(Door2)
 	Door2.setTargetDoor(Door1)
